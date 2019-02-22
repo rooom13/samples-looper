b01c03404fec74cb5eeeb7a6e7353a50f482e860
rooom13 <romanreypadrero@gmail.com>
2019-02-22 11:58:36 +0100

    arrow control

--- client/src/components/Turntable/Turntable.js
+++ client/src/components/Turntable/Turntable.js
@@ -1,6 +1,6 @@
 import React, { Component, Fragment } from 'react';
 // import styled from 'styled-components'
-import KeyHandler, { KEYPRESS } from 'react-key-handler'
+import KeyHandler, { KEYPRESS, KEYDOWN } from 'react-key-handler'
 import Disk from './Disk/Disk'
 
 
@@ -13,30 +13,47 @@ class Turntable extends Component {
 
     selectDisk = (ev, disk) => {
         ev.preventDefault()
-        this.setState({ selectedDisk: disk })
+        if(disk < 0 ) disk = this.props.disks.length
+        else if(disk > this.props.disks.length ) disk = 0
+        
+        this.setState({ selectedDisk: disk  })
     }
     render() {
         let diskCount = 0
         const { selectedDisk } = this.state
         return (
-            this.props.disks.map(loop => {
-                const diskIndex = diskCount
-                diskCount++
-                return (<Fragment key={loop}>
-                    <KeyHandler
-                        keyEventName={KEYPRESS}
-                        keyValue={(diskIndex + 1).toString()}
-                        onKeyHandle={(ev) => this.selectDisk(ev, diskIndex)}
-                    />
 
-                    <Disk
-                        src={loop}
-                        index={diskIndex + 1}
-                        isSelected={selectedDisk === diskIndex} >{loop}</Disk>
-                </Fragment>)
-            }
+            <Fragment>
+                 <KeyHandler
+                            keyEventName={KEYDOWN}
+                            code={('ArrowDown').toString()}
+                            onKeyHandle={(ev) => this.selectDisk(ev, selectedDisk +1 )}
+                        />
+                 <KeyHandler
+                            keyEventName={KEYDOWN}
+                            code={('ArrowUp').toString()}
+                            onKeyHandle={(ev) => this.selectDisk(ev, selectedDisk - 1 )}
+                        />
 
-            )
+                {this.props.disks.map(loop => {
+                    const diskIndex = diskCount
+                    diskCount++
+                    return (<Fragment key={loop}>
+                        <KeyHandler
+                            keyEventName={KEYPRESS}
+                            keyValue={(diskIndex + 1).toString()}
+                            onKeyHandle={(ev) => this.selectDisk(ev, diskIndex)}
+                        />
+
+                        <Disk
+                            src={loop}
+                            index={diskIndex + 1}
+                            isSelected={selectedDisk === diskIndex} >{loop}</Disk>
+                    </Fragment>)
+                }
+
+                )}
+            </Fragment>
 
         )
     }
